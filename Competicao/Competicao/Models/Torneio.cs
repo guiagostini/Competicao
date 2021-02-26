186d644c90e032eb3aa00fd2a8cb2f289b1453c6
Guilherme de Agostini da Silva <guilher.agostini@gmail.com>
2021-02-26 09:44:51 -0300

    Criando propriedade Modificacao em Torneio

--- Competicao/Competicao/Models/Torneio.cs
+++ Competicao/Competicao/Models/Torneio.cs
@@ -11,6 +11,7 @@ namespace Competicao.Models
         public long? ID { get; set; }
         public string Nome { get; set; }
         public DateTime Criacao { get; } = DateTime.Now;
+        public DateTime Modificacao { get; set; }
 
         public virtual ICollection<Time> Times { get; set; }
     }
